a37f5eef2c20434844ca01d9ef3a00db2a1e0f0f
Daitarus <chajkovskij_99@mail.ru@>
2023-02-08 14:21:21 +0300

    Rename solution and add RepositoryDB

--- LibraryBot/Program.cs
+++ LibraryBot/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace Library_bot
+﻿namespace LibraryBot
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //TODO Main :)
         }
     }
 }
