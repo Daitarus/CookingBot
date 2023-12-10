c40322679eacbcb605e657421756ccd5f31802e5
Daitarus <38458173+Daitarus@users.noreply.github.com>
2023-12-10 12:51:59 +0300

    refactor: add Start, AddRecipe and delete old commands

---
+++ CookingBot/BotBehaviors/Requests/Commands/AddRecipe.cs
@@ -0,0 +1,56 @@
+﻿using CookingBot.BotBehaviors.Responses;
+using System.Text;
+using CookingBot.BotBehaviors.Requests.Interfaces;
+using CookingBotDB.Entities;
+using CookingBotDB.Contexts;
+
+namespace CookingBot.BotBehaviors.Requests.Commands
+{
+    internal class AddRecipe : IRequest
+    {
+        public const string CommandValue = "/add_recipe";
+
+        private const UserState _assignableUserState = UserState.AddRecipe;
+
+        private DbContextFactory _dbContextFacoty;
+        private User _user;
+
+        public AddRecipe(DbContextFactory dbContextFacoty, User user)
+        {
+            if (dbContextFacoty == null)
+                throw new ArgumentNullException(nameof(dbContextFacoty));
+            if (user == null)
+                throw new ArgumentNullException(nameof(user));
+
+            _dbContextFacoty = dbContextFacoty;
+            _user = user;
+        }
+
+        public void Execute()
+        {
+            _user.State = _assignableUserState;
+
+            using (var context = _dbContextFacoty.Create())
+            {
+                var user = context.Users.FirstOrDefault(u => u.Id == _user.Id);
+                user = _user;
+                context.SaveChanges();
+            }
+        }
+
+        public IResponse CreateResponse()
+        {
+            return new Response(CreateResponseText());
+        }
+
+        //TODO Get ResponseText from file
+        private string CreateResponseText()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("Отправте имя рецепта и сам рецепт текстом с следующий строки или текстовым файлом.\n");
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ CookingBot/BotBehaviors/Requests/Commands/Start.cs
@@ -0,0 +1,60 @@
+﻿using CookingBot.BotBehaviors.Requests.Interfaces;
+using CookingBot.BotBehaviors.Responses;
+using CookingBotDB.Contexts;
+using CookingBotDB.Entities;
+using System.Text;
+
+namespace CookingBot.BotBehaviors.Requests.Commands
+{
+    internal class Start : IRequest
+    {
+        public const string CommandValue = "/start";
+
+        private const UserState _assignableUserState = UserState.Initial;
+
+        private DbContextFactory _dbContextFacoty;
+        private User _user;
+
+        public Start(DbContextFactory dbContextFacoty, User user)
+        {
+            if (dbContextFacoty == null)
+                throw new ArgumentNullException(nameof(dbContextFacoty));
+            if (user == null)
+                throw new ArgumentNullException(nameof(user));
+
+            _dbContextFacoty = dbContextFacoty;
+            _user = user;
+        }
+
+        public void Execute()
+        {
+            _user.State = _assignableUserState;
+
+            using (var context = _dbContextFacoty.Create())
+            {
+                var user = context.Users.FirstOrDefault(u => u.Id == _user.Id);
+                user = _user;
+                context.SaveChanges();
+            }
+        }
+
+        public IResponse CreateResponse()
+        {
+            return new Response(CreateResponseText());
+        }
+
+        //TODO Get ResponseText from file
+        private string CreateResponseText()
+        {
+            string userName = !string.IsNullOrEmpty(_user.UserName) ? _user.UserName : _user.FirstName;
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("Hello ");
+            stringBuilder.Append(userName);
+            stringBuilder.Append("! Wellcome to bot.");
+
+            return stringBuilder.ToString();
+        }
+    }
+}
