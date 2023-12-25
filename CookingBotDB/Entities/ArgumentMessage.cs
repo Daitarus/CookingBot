63c365eea1f5e6f485e8bb0810c7c20170a7187c
Daitarus <38458173+Daitarus@users.noreply.github.com>
2023-12-25 23:36:37 +0300

    feat: add ArgumentMessage

---
+++ CookingBotDB/Entities/ArgumentMessage.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CookingBotDB.Entities
+{
+    [Table("AgrumentMessages")]
+    public class ArgumentMessage
+    {
+        [Column("id"), Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column("user_id"), Required]
+        public int UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+
+        [Column("message"), Required]
+        public string Message { get; set; }
+    }
+}
