f4b9ff8dd4de05f5a109d7ebb580ebacbb921e66
Daitarus <38458173+Daitarus@users.noreply.github.com>
2024-02-03 23:43:03 +0300

    fix: change contextFactory into UserRequest

--- CookingBot/BotBehaviors/Requests/Undefined/UserRequest.cs
+++ CookingBot/BotBehaviors/Requests/Undefined/UserRequest.cs
@@ -6,31 +6,25 @@ namespace CookingBot.BotBehaviors.Requests.Undefined
 {
     public class UserRequest : Request
     {
-        protected UserState _assignableUserState;
-
-        protected DbContextFactory _dbContextFacoty;
+        protected DbContextFactory _contextFactory;
         protected User _user;
         protected ILogger? _logger;
 
-        public UserRequest(DbContextFactory dbContextFacoty, User user, ILogger? logger = null)
+        public UserRequest(DbContextFactory contextFactory, User user, ILogger? logger = null)
         {
-            if (dbContextFacoty == null)
-                throw new ArgumentNullException(nameof(dbContextFacoty));
+            if (contextFactory == null)
+                throw new ArgumentNullException(nameof(contextFactory));
             if (user == null)
                 throw new ArgumentNullException(nameof(user));
 
-            _assignableUserState = UserState.Initial;
-
-            _dbContextFacoty = dbContextFacoty;
+            _contextFactory = contextFactory;
             _user = user;
             _logger = logger;
         }
 
         public override void Execute()
         {
-            _user.State = _assignableUserState;
-
-            using (var context = _dbContextFacoty.Create())
+            using (var context = _contextFactory.Create())
             {
                 var user = context.Users.FirstOrDefault(u => u.Id == _user.Id);
                 user = _user;
