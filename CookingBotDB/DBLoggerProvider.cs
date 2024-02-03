a34b3f92c5a342ada57b7715a8e06396d9e81ca1
Daitarus <38458173+Daitarus@users.noreply.github.com>
2024-02-03 23:41:58 +0300

    fix: add loggerProvider for adding logger into dbOptions

---
+++ CookingBotDB/DBLoggerProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CookingBotDB
+{
+    public class DBLoggerProvider : ILoggerProvider
+    {
+        private ILogger _logger;
+
+        public DBLoggerProvider(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return _logger;
+        }
+
+        public void Dispose() { }
+    }
+}
